1c3fe5784bf15f260994f5922afb1edebd94a915
seola12e <dlcodus1201@naver.com>
2026-04-17 00:41:33 +0900

    OrderResponse 오류로 인한 반환 객체 수정

--- src/main/java/com/sparta/miniorder/order/dto/OrderResponse.java
+++ src/main/java/com/sparta/miniorder/order/dto/OrderResponse.java
@@ -1,17 +1,23 @@
 package com.sparta.miniorder.order.dto;
 
 import com.sparta.miniorder.order.entity.Order;
-import com.sparta.miniorder.product.entity.Product;
 import lombok.Getter;
 
 @Getter
 public class OrderResponse {
 
     private final Long orderId;
-    private final Product product;
+    /**
+     * Product 관련 반환은 productId, productName, productPrice 로 한다.
+     */
+    private final Long productId;
+    private final String productName;
+    private final Integer productPrice;
 
     public OrderResponse(Order saved) {
         this.orderId = saved.getId();
-        this.product = saved.getProduct();
+        this.productId = saved.getProduct().getId();
+        this.productName = saved.getProduct().getName();
+        this.productPrice = saved.getProduct().getPrice();
     }
 }
